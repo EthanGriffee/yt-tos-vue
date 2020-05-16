832aa075b2123ea67e93bbb1106788982dd2cc32
Ethan Griffee <griffee.e@husky.neu.edu>
2020-05-16 17:38:12 -0400

    initial commit

---
+++ src/components/Game.vue
@@ -0,0 +1,27 @@
+<template>
+    <div>
+        <div v-for="player in players" :key="player.id">
+
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'Game',
+  props: {
+    players: Array
+  },
+  methods: {
+    async getEmployees() {
+        try {
+            const response = await fetch('https://jsonplaceholder.typicode.com/users')
+            const data = await response.json()
+            this.players = data
+        } catch (error) {
+            console.error(error)
+        }
+    }
+  }
+}
+</script>
