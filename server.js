90c9f68233595c5b6757ba9a65bfc1e3776bc7d1
Ethan Griffee <griffee.e@husky.neu.edu>
2020-06-24 14:41:36 -0400

    split app.use to use cors

--- server.js
+++ server.js
@@ -4,10 +4,17 @@ var cors = require('cors');
 var path = require('path');
 var serveStatic = require('serve-static');
 app = express();
-app.use(serveStatic(__dirname + "/dist"), cors({
-    origin: 'https://generalpeanut.herokuapp.com/'
-}));
+app.use(serveStatic(__dirname + "/dist"));
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin",
+        "https://generalpeanut.herokuapp.com/");
+    res.header("Access-Control-Allow-Headers",
+        "Origin, X-Requested-With, Content-Type, Accept");
+    res.header("Access-Control-Allow-Methods",
+        "GET, POST, PUT, DELETE, OPTIONS");
+    res.header("Access-Control-Allow-Credentials", "true");
+    next();
+});
 var port = process.env.PORT || 5000;
 app.listen(port);
 console.log('server started '+ port);
-
