8a5906b5476ad64fe080b4829ba015806786f542
Ethan Griffee <griffee.e@husky.neu.edu>
2020-06-24 13:16:21 -0400

    added cors

--- server.js
+++ server.js
@@ -1,9 +1,13 @@
 // server.js
 var express = require('express');
+var cors = require('cors');
 var path = require('path');
 var serveStatic = require('serve-static');
 app = express();
-app.use(serveStatic(__dirname + "/dist"));
+app.use(serveStatic(__dirname + "/dist"), cors({
+    origin: 'https://generalpeanut.herokuapp.com/'
+}));
 var port = process.env.PORT || 5000;
 app.listen(port);
 console.log('server started '+ port);
+
