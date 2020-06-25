609a65543695d47d9e29f4d17aeaccd21152c373
Ethan Griffee <griffee.e@husky.neu.edu>
2020-06-25 01:47:33 -0400

    figured out why CORS wasnt working... didnt type the full URL into the env file...

--- server.js
+++ server.js
@@ -1,20 +1,9 @@
 // server.js
 var express = require('express');
-var cors = require('cors');
 var path = require('path');
 var serveStatic = require('serve-static');
 app = express();
 app.use(serveStatic(__dirname + "/dist"));
-app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin",
-        "https://generalpeanut.herokuapp.com/");
-    res.header("Access-Control-Allow-Headers",
-        "Origin, X-Requested-With, Content-Type, Accept");
-    res.header("Access-Control-Allow-Methods",
-        "GET, POST, PUT, DELETE, OPTIONS");
-    res.header("Access-Control-Allow-Credentials", "true");
-    next();
-});
 var port = process.env.PORT || 5000;
 app.listen(port);
 console.log('server started '+ port);
